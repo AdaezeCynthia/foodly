ec2de3eb0f74f51bda0384f48a9680b46d8926a7
unknown <nackycynthia@gmail.com>
2023-09-01 11:43:46 +0100

    1st sept commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar";
 import  Hero from './Components/Hero';
+import Delivery from './Components/Delivery'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
           <Navbar/>
           <Hero/>
+          <Delivery/>
     </div>
   );
 }
--- src/Components/Delivery.js
+++ src/Components/Delivery.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
+
 const Delivery = () => {
   return (
-    <div>Delivery</div>
+    <div className='w[100%] bg-white py-16 px-4' >
+
+      <h3 className='text-red-700  font-bold  text-2xl text-center'>
+               Recipe
+      </h3>
+      <div className=' flex    flex-row' >
+                
+                   <div className='w-1/2 h-[20%] mx-3 my-4 rounded-md ' >
+                    <img  className='rounded-md' src='https://res.cloudinary.com/dbkc6y0cy/image/upload/v1693181045/assetss/chef_sqpyjr.png'/>
+                    </div>
+                   
+                    <div className='w-1/2 py-[30%]'>
+                    <p className='text-orange-700  font-bold'>Get Cooking</p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience</h1>
+                      <p>
+                        ggggggggggggggggggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+                        jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                        jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                      </p>
+                      <button className='bg-black text-orange-700 w-[200px] rounded-md
+                      font-medium my-6 mx-auto md:mx-0  py-3'> Get Started</button>
+                      </div>
+                   </div>
+    </div>
+
+
+    
   )
 }
 
